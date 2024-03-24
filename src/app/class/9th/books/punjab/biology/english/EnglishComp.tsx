8f5634d558d8cbdabe25aa9aa4b53cb5ae52c66d
Muzammil327 <mmuzammiloff327@gmail.com>
2024-03-24 11:51:30 +0500

    updated robot.txt

--- src/app/class/9th/books/punjab/biology/english/EnglishComp.tsx
+++ src/app/class/9th/books/punjab/biology/english/EnglishComp.tsx
@@ -7,17 +7,14 @@ export default function EnglishComp() {
   const fileName = 'class-9-punjab-board-biology-book-english-medium.pdf'
   const fileId = './class-9-punjab-board-biology-book-english-medium.pdf'
 
-  // Function to handle iframe load event
   const handleIframeLoad = () => {
-    setIsLoading(false) // Set isLoading to false once the iframe content is loaded
+    setIsLoading(false)
   }
 
   return (
     <>
       {isLoading && (
         <div className="bg-animation-container">
-          {/* Your background animation component */}
-          {/* Replace this with your actual animation */}
           <div className="bg-animation">Loading...</div>
         </div>
       )}
@@ -28,7 +25,7 @@ export default function EnglishComp() {
         className={`w-full px-2 lg:h-[900px] md:h-[800] h-[600px] ${isLoading ? 'hidden' : ''}`}
         height={900}
         width={900}
-        onLoad={handleIframeLoad} // Call handleIframeLoad when the iframe is loaded
+        onLoad={handleIframeLoad}
       ></iframe>
       <FileDownloader fileId={fileId} fileName={fileName} />
     </>
