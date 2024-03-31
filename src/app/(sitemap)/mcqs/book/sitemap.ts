a9effc4846e14ca2bd67664bb4d9439e889be4c4
Muzammil327 <mmuzammiloff327@gmail.com>
2024-03-31 15:35:57 +0500

    updated sitemap

--- src/app/(sitemap)/mcqs/book/sitemap.ts
+++ src/app/(sitemap)/mcqs/book/sitemap.ts
@@ -1,12 +1,9 @@
-import { MetadataRoute } from 'next'
 import { getData1 } from '@/src/app/(sitemap)/mcqs/book/fetchdata'
 
-export default async function sitemap({}: {}): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap() {
   const datas = await getData1()
   return datas.user.map((product: any) => ({
     url: `${process.env.NEXT_URL}/quiz/${product.slug}`,
     lastModified: new Date(),
-    changeFrequency: 'weekly',
-    priority: 0.9,
   }))
 }
