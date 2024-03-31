9aca365968b915cff89c68846928fd9768468bf2
Muzammil327 <mmuzammiloff327@gmail.com>
2024-03-31 15:46:06 +0500

    updated sitemap

--- src/app/(sitemap)/mcqs/book/sitemap.ts
+++ src/app/(sitemap)/mcqs/book/sitemap.ts
@@ -1,8 +1,8 @@
 import { getData1 } from '@/src/app/(sitemap)/mcqs/book/fetchdata'
 
 export default async function sitemap() {
-  const datas = await getData1()
-  return datas.user.map((product: any) => ({
+  const data = await getData1()
+  return data.user.map((product: any) => ({
     url: `${process.env.NEXT_URL}/quiz/${product.slug}`,
     lastModified: new Date(),
   }))
