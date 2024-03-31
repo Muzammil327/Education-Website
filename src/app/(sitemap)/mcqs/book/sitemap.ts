8229fce598f9ce0ea0f59bf0e9870e5e1824faee
Muzammil327 <mmuzammiloff327@gmail.com>
2024-03-31 13:58:08 +0500

    updated the full quiz route fixed some issue

--- src/app/(sitemap)/mcqs/book/sitemap.ts
+++ src/app/(sitemap)/mcqs/book/sitemap.ts
@@ -9,21 +9,15 @@ export default async function sitemap({
   id,
 }: {
   id: number
-}): Promise<MetadataRoute.Sitemap[]> {
+}): Promise<MetadataRoute.Sitemap> {
   const datas = await getData1()
   const start = id * 50000
   const end = start + 49999
-
-  const slicedData = datas.user.slice(start, end + 1) // Add 1 to include the end index
-  console.log(slicedData.length)
-  // Map sliced data to sitemap format
-
-  const sitemapEntries = slicedData.map((data: any) => ({
-    url: `${process.env.NEXT_URL}/quiz/${data.slug}/`,
+  const slicedData = datas.user.slice(start, end + 1)
+  return slicedData.map((product: any) => ({
+    url: `${process.env.NEXT_URL}/quiz/${product.slug}`,
     lastModified: new Date(),
     changeFrequency: 'weekly',
-    priority: 0.8,
+    priority: 0.9,
   }))
-
-  return sitemapEntries
 }
