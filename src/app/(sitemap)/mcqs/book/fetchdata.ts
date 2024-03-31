bf8776108c3191a077839b35a26dc6915f7e8941
Muzammil327 <mmuzammiloff327@gmail.com>
2024-03-31 16:48:47 +0500

    updated sitemap

--- src/app/(sitemap)/mcqs/book/fetchdata.ts
+++ src/app/(sitemap)/mcqs/book/fetchdata.ts
@@ -1,20 +1,32 @@
+// export async function getData1() {
+//   try {
+//     const response = await fetch(`https://www.codebloglab.website/api/book`, {
+//       // next: { revalidate: 3600 },
+//       cache: 'no-store',
+//     })
+//     if (!response.ok) {
+//       throw new Error('Failed to fetch data')
+//     }
+//     const data = await response.json() // Parse the JSON data
+
+//     return data
+//   } catch (error: any) {
+//     if (error instanceof Error) {
+//       throw new Error('Failed to fetch data: ' + error.message)
+//     } else {
+//       throw new Error('An unknown error occurred while fetching data')
+//     }
+//   }
+// }
+import axios from 'axios'
+
 export async function getData1() {
   try {
-    const response = await fetch(`https://www.codebloglab.website/api/book`, {
-      // next: { revalidate: 3600 },
-      cache: 'no-store',
-    })
-    if (!response.ok) {
-      throw new Error('Failed to fetch data')
-    }
-    const data = await response.json() // Parse the JSON data
-
-    return data
-  } catch (error: any) {
-    if (error instanceof Error) {
-      throw new Error('Failed to fetch data: ' + error.message)
-    } else {
-      throw new Error('An unknown error occurred while fetching data')
-    }
+    const response = await axios.get(`/api/book`)
+    console.log(response)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    throw new Error('Failed to fetch data')
   }
 }
--- src/app/(sitemap)/mcqs/book/sitemap.ts
+++ src/app/(sitemap)/mcqs/book/sitemap.ts
@@ -1,12 +1,19 @@
-import { getData1 } from '@/src/app/(sitemap)/mcqs/book/fetchdata'
-
 export default async function sitemap() {
-  const data = await getData1()
-  console.log(data)
-  const posts = data.user.map((data: any) => ({
-    url: `https://www.codebloglab.website/quiz/${data.slug}`,
-    lastModified: new Date(),
-  }))
+  let posts = [] // Default value for posts
 
-  return [...posts]
+  try {
+    let data = await fetch('https://codebloglab.website/api/book')
+    let datapost = await data.json()
+
+    posts = datapost.user.map((item: any) => ({
+      url: `https://www.codebloglab.website/quiz/${item.slug}`,
+      lastModified: new Date(),
+    }))
+
+    return posts
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    // Handle errors appropriately
+    return posts // Return default value
+  }
 }
