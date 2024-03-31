0260fe62bf04306bd3b82f618041b688e5eff88d
Muzammil327 <mmuzammiloff327@gmail.com>
2024-03-31 15:30:51 +0500

    updated sitemap

--- src/app/(sitemap)/mcqs/book/sitemap.ts
+++ src/app/(sitemap)/mcqs/book/sitemap.ts
@@ -1,20 +1,9 @@
 import { MetadataRoute } from 'next'
 import { getData1 } from '@/src/app/(sitemap)/mcqs/book/fetchdata'
 
-export async function generateSitemaps() {
-  return [{ id: 0 }, { id: 1 }]
-}
-
-export default async function sitemap({
-  id,
-}: {
-  id: number
-}): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap({}: {}): Promise<MetadataRoute.Sitemap> {
   const datas = await getData1()
-  const start = id * 50000
-  const end = start + 49999
-  const slicedData = datas.user.slice(start, end + 1)
-  return slicedData.map((product: any) => ({
+  return datas.user.map((product: any) => ({
     url: `${process.env.NEXT_URL}/quiz/${product.slug}`,
     lastModified: new Date(),
     changeFrequency: 'weekly',
